509fe7b3488fc770aeeacf6d997e1e29d12e5418
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-03-27 01:09:33 +0100

    code reformat

--- exif/actions.c
+++ exif/actions.c
@@ -189,8 +189,11 @@ show_entry_machine (ExifEntry *e, void *data)
 	unsigned char *ids = data;
 	char v[1024];
 
-	if (*ids) fprintf (stdout, "0x%04x", e->tag);
-	else fputs (CN (exif_tag_get_title (e->tag)), stdout);
+	if (*ids) {
+		fprintf (stdout, "0x%04x", e->tag);
+	} else {
+		fputs (CN (exif_tag_get_title (e->tag)), stdout);
+	}
 	fputc ('\t', stdout);
 	puts (CN (exif_entry_get_value (e, v, sizeof (v))), stdout);
 	fputc ('\n', stdout);
