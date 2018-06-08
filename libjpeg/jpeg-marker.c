dbeffd4f128a10af5cd076ee2a133c30c6adff66
Dan Fandrich <dan@coneharvesters.com>
2018-06-08 21:24:45 +0200

    Got rid of non ASCII characters in code comments.

--- libjpeg/jpeg-marker.c
+++ libjpeg/jpeg-marker.c
@@ -1,6 +1,6 @@
 /* jpeg-marker.c
  *
- * Copyright © 2001-2008 Lutz Müller <lutz@users.sourceforge.net>
+ * Copyright (c) 2001-2008 Lutz Mueller <lutz@users.sourceforge.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
