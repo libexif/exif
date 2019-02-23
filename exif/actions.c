1e497e9acaf30a45448ec62dca57949341c8f360
Dan Fandrich <dan@coneharvesters.com>
2019-02-23 09:59:13 +0100

    Make sure the tag buffer in show_entry_xml is NUL-terminated.
    None of the tags are large enough to fill the buffer so this doesn't
    actually lead to a problem, but it keeps Coverity happy.

--- exif/actions.c
+++ exif/actions.c
@@ -687,6 +687,7 @@ show_entry_xml (ExifEntry *e, void *data)
 		fprintf (stdout, "</x%04x>", e->tag);
 	} else {
 		strncpy (t, exif_tag_get_title_in_ifd(e->tag, exif_entry_get_ifd(e)), sizeof (t));
+		t[sizeof(t)-1] = 0;
 
 		/* Remove invalid characters from tag eg. (, ), space */
 		remove_bad_chars(t);
