bd5229697ec9dd3991117b87a1ce81bedce31a02
Hugh McMaster <hugh.mcmaster@outlook.com>
2022-09-05 14:26:19 +1000

    exif/actions.c: Return empty string on NULL pointer or zero-length string
    Updates and optimizes existing fix for CVE-2021-27815.

--- exif/actions.c
+++ exif/actions.c
@@ -666,7 +666,7 @@ escape_xml(const char *text)
 	char *out;
 	size_t len;
 
-	if (!strlen(text)) return "";
+	if (!text || !*text) return "";
 
 	for (out=escaped, len=0; *text; ++len, ++out, ++text) {
 		/* Make sure there's plenty of room for a quoted character */
